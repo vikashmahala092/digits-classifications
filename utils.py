332a83d52ad721f22d0d6371c2933057e3a21bfb
vikashmahala092 <vikashmahala092@gmail.com>
2023-09-23 15:05:21 +0530

    Test case changes

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def preprocess_data(data):
 # Split data into 50% train and 50% test subsets
 def split_data(x, y, test_size, random_state=1):
     X_train, X_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.5, shuffle=False, random_state=random_state
+        x, y, test_size=test_size, shuffle=False, random_state=random_state
     )
     return X_train, X_test, y_train, y_test
 
